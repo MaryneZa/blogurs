c019d2d9f95dee30aff09827d8998fa4af348808
MaryneZa <maryne6916@gmail.com>
2025-05-28 23:59:18 +0700

    fix header

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,16 +1,19 @@
 const {jwt} = require('../utils/index')
 
 function authMiddleware(req, res, next) {
-    const token = req.header('Authorization');
-    if (!token) {
-        res.status(401).json({ error: 'Access denied' });
+    const authHeader = req.header('Authorization');
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'Access denied: No token provided' });
     }
+
+    const token = authHeader.split(' ')[1];
     try {
         const decode = jwt.VerifyToken(token)
         req.username = decode.username
         next()
     } catch (error) {
-        res.status(401).json({ error: 'Invalid token' });
+        res.status(401).json({ error: 'Invalid token:' });
     }
 }
 
